8512891c9bc9da7a2cac654c38245aa29a9d94c4
Weldon Kiprop <118200102+Lagat100@users.noreply.github.com>
2024-02-20 23:56:01 +0300

    Update functions2.c

--- functions2.c
+++ functions2.c
@@ -140,9 +140,7 @@ int print_reverse(va_list types, char buffer[],
  * @types: Lista of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specifier
+ * @width: 
  * Return: Numbers of chars printed
  */
 int print_rot13string(va_list types, char buffer[],
